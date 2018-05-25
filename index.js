717d1c1e87b4d3de960a81f769f69222add8f466
Isabella <burbanojaime98@gmail.com>
2018-05-25 15:53:35 -0500

    funciona el txt

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express'),
     consolidate = require('consolidate'),
     hbs = require('handlebars'),
-    fb = require('fs');
+    fs = require('fs');
 
 var app = express();
 
@@ -18,13 +18,52 @@ var proyectos = 0;
 var contacto = 0;
 
 app.get('/',(req,res) =>{
+
+    var inicio =+ 1;
+    var proyectos = proyectos;
+    var contacto = contacto;
+
+    fs.writeFile('./visitas.txt', '{\n"Inicio": ' + inicio + ';\n"Proyectos": ' + proyectos + ';\n"Contacto": ' + contacto + ';\n}', error => {
+        if (error)
+            console.log(error);
+        else
+            console.log('El archivo fue creado');
+        console.log(inicio);
+    });
+
     res.render('index');
 });
 
-app.get('/paginaUno',(req,res) =>{
-    res.render('pagUno');
+app.get('/proyectos',(req,res) =>{
+
+    var inicio = inicio;
+    var proyectos =+1;
+    var contacto = contacto;
+
+    fs.writeFile('./visitas.txt', '{\n"Inicio": ' + inicio + ';\n"Proyectos": ' + proyectos + ';\n"Contacto": ' + contacto + ';\n}', error => {
+        if (error)
+            console.log(error);
+        else
+            console.log('El archivo fue creado');
+        console.log(inicio);
+    });
+
+    res.render('proyectos');
 });
 
-app.get('/paginaDos',(req,res) =>{
-    res.render('pagDos');
+app.get('/contacto',(req,res) =>{
+
+    var inicio = inicio;
+    var proyectos = proyectos;
+    var contacto =+1;
+
+    fs.writeFile('./visitas.txt', '{\n"Inicio": ' + inicio + ';\n"Proyectos": ' + proyectos + ';\n"Contacto": ' + contacto + ';\n}', error => {
+        if (error)
+            console.log(error);
+        else
+            console.log('El archivo fue creado');
+        console.log(inicio);
+    });
+
+    res.render('contacto');
 });
